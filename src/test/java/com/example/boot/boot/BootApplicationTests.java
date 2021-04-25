6e7ff464f2ec41fb5b6841115608fb26a3ae3509
zziny <cheongsu030536@gmail.com>
2021-04-25 13:47:24 +0900

    스프링부트 aws배포까지

---
+++ src/test/java/com/example/boot/boot/BootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
